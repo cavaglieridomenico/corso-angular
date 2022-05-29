7f3a3731e42ab0036049e9264fb025b11986ae5a
cavaglieridomenico <cavaglieridomenico@gmail.com>
2022-05-29 19:21:35 +0200

    [FEAT] Update lista-ricette component

--- ANGULAR/proj0Ver1/src/app/ricette/lista-ricette/lista-ricette.component.ts
+++ ANGULAR/proj0Ver1/src/app/ricette/lista-ricette/lista-ricette.component.ts
@@ -33,17 +33,30 @@ export class ListaRicetteComponent implements OnInit {
   constructor() {
     this.ricette = [
       {
-        nome: 'Pasta al pesto',
-        descrizione: 'da Genova con amore',
+        nome: 'Pasta e fagioli',
+        descrizione: 'tipico piatto italiano',
         prezzo: 6,
         image: 'https://via.placeholder.com/150',
       },
       {
-        nome: 'Spagetti allo scoglio',
+        nome: 'Pasta al pesto',
         descrizione: 'da Genova con amore',
-        prezzo: 6,
+        prezzo: 9,
+        image: 'https://via.placeholder.com/150',
+      },
+      {
+        nome: 'Spagetti allo scoglio',
+        descrizione: 'pietre non comprese',
+        prezzo: 12,
         image: 'https://via.placeholder.com/150',
         allergeni: ['cozze', 'ricci', 'vongole'],
+      },
+      {
+        nome: 'Carbonara',
+        descrizione: 'da Mangiare solo a Roma',
+        prezzo: 12,
+        image: 'https://via.placeholder.com/150',
+        allergeni: ['uova'],
       },
     ];
   }
