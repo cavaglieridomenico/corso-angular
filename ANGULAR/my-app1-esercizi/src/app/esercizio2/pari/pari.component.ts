3d326722ad76675b7bc2a712b4291b9dc563d783
cavaglieridomenico <cavaglieridomenico@gmail.com>
2022-06-02 15:52:34 +0200

    [FEAT] Complete Pari Component

---
+++ ANGULAR/my-app1-esercizi/src/app/esercizio2/pari/pari.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pari',
+  templateUrl: './pari.component.html',
+  styleUrls: ['./pari.component.css']
+})
+export class PariComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
