0c21e05599fcd1095a29a8de257782f5d4dd26d2
cavaglieridomenico <cavaglieridomenico@gmail.com>
2022-06-02 15:51:42 +0200

    [FEAT] Setup esercizio2

--- ANGULAR/my-app1-esercizi/src/app/app.module.ts
+++ ANGULAR/my-app1-esercizi/src/app/app.module.ts
@@ -6,6 +6,10 @@ import { Esercizio1Component } from './01-01062022/esercizio1/esercizio1.compone
 import { ButtonComponent } from './01-01062022/esercizio1/button/button.component';
 import { ParagraphComponent } from './01-01062022/esercizio1/paragraph/paragraph.component';
 import { DisplayCounterComponent } from './01-01062022/esercizio1/display-counter/display-counter.component';
+import { Esercizio2Component } from './esercizio2/esercizio2.component';
+import { PariComponent } from './esercizio2/pari/pari.component';
+import { DispariComponent } from './esercizio2/dispari/dispari.component';
+import { GameControlComponent } from './esercizio2/game-control/game-control.component';
 
 @NgModule({
   declarations: [
@@ -14,6 +18,10 @@ import { DisplayCounterComponent } from './01-01062022/esercizio1/display-counte
     ButtonComponent,
     ParagraphComponent,
     DisplayCounterComponent,
+    Esercizio2Component,
+    PariComponent,
+    DispariComponent,
+    GameControlComponent,
   ],
   imports: [BrowserModule],
   providers: [],
