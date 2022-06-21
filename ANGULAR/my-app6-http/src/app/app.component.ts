1982804d6dcb9b9a96e2c374510b3516fc2dfa4b
cavaglieridomenico <cavaglieridomenico@gmail.com>
2022-06-21 10:00:11 +0200

    [FEAT] App7-material

--- ANGULAR/my-app6-http/src/app/app.component.ts
+++ ANGULAR/my-app6-http/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { UsersService } from './users.service';
+import { Utente } from './utente.model';
 
 @Component({
   selector: 'app-root',
@@ -10,9 +11,15 @@ export class AppComponent {
   title = 'my-app6-http';
   constructor(private userService: UsersService) {}
 
+  mieiUsers: Utente[];
+
   onPrendiUtenti() {
     this.userService.getData().subscribe((utenti) => {
       console.log(utenti);
+    });
+    this.userService.getDataResponse().subscribe((utenti) => {
+      console.log(utenti);
+      console.log(utenti.body);
     });
   }
 
@@ -23,5 +30,26 @@ export class AppComponent {
     this.userService.fetchUtenti().subscribe((utenti) => {
       console.log(utenti);
     });
+  }
+
+  onCancellaUtenti() {
+    this.userService.deleteUsers().subscribe(() => {
+      console.log('Hai cancellato tutti gli utenti');
+      this.mieiUsers = [];
+    });
+  }
+  onCancellaSingolo(id: string) {
+    this.userService.deleteSingoloUser(id).subscribe(() => {
+      console.log("Cancellato l'utente con id: " + id);
+      this.onPrendiMieiUtenti();
+    });
+  }
+
+  onGetPost() {
+    this.userService.getPost().subscribe((response) => console.log(response));
+  }
+
+  onPostDataPost() {
+    this.userService.postDataPost();
   }
 }
